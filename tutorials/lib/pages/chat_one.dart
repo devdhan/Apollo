c17013b5c01aa4fde876d05afbbbe28eb44047d2
Daniel <danielbolaji99@gmail.com>
2024-08-18 14:52:54 +0100

    chat update

--- tutorials/lib/pages/chat_one.dart
+++ tutorials/lib/pages/chat_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutorials/components/my_button.dart';
 import 'package:tutorials/pages/about_app.dart';
 import 'package:tutorials/pages/search_history.dart';
 import 'dart:math' as math;
@@ -43,6 +44,7 @@ class ChatOne extends StatelessWidget {
               ),
             ],
           ),
+          //APPBAR
           child: AppBar(
             backgroundColor: Colors.transparent,
             elevation: 0,
@@ -90,6 +92,7 @@ class ChatOne extends StatelessWidget {
           ),
         ),
       ),
+      //DRAWER
       drawer: Drawer(
         child: Container(
           color: const Color(0xFFEAE3D1),
@@ -188,6 +191,7 @@ class ChatOne extends StatelessWidget {
           ),
         ),
       ),
+      //BODY
       backgroundColor: const Color.fromRGBO(234, 227, 209, 1),
       body: SafeArea(
         child: Stack(
@@ -198,88 +202,134 @@ class ChatOne extends StatelessWidget {
               width: double.infinity,
               height: double.infinity,
             ),
-            const Positioned.fill(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 100.0),
-                  child: Text(
-                    'Hello, there\nHow can I help you\ntoday?',
-                    style: TextStyle(
-                      fontSize: 34,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w700,
-                      color: Color(0xFF11100B),
-                    ),
+            const Positioned(
+              top: 100.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  'Hello, there\nHow can I help you\ntoday?',
+                  style: TextStyle(
+                    fontSize: 34,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF11100B),
                   ),
                 ),
               ),
             ),
-            const Positioned.fill(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 300.0),
-                  child: Text(
-                    'Suggestions:',
-                    style: TextStyle(
-                      fontSize: 17,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF000000),
-                    ),
+            const Positioned(
+              top: 300.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Text(
+                  'Suggestions:',
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF000000),
                   ),
                 ),
               ),
             ),
-            Positioned.fill(
+            Positioned(
+              top: 340.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: MyButton(
+                    onTap: () {},
+                    buttonText: 'Can you explain the theory of evolution?',
+                    buttoncolor: const Color.fromRGBO(17, 16, 11, 1),
+                    buttonTextColor: const Color(0xFFEAE3D1),
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 410.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: MyButton(
+                    onTap: () {},
+                    buttonText: 'How do you solve quadratic equations?',
+                    buttoncolor: const Color.fromRGBO(17, 16, 11, 1),
+                    buttonTextColor: const Color(0xFFEAE3D1),
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 480.0,
+              left: 0,
+              right: 0,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: MyButton(
+                    onTap: () {},
+                    buttonText: 'What are Newton’s three laws of motion?',
+                    buttoncolor: const Color.fromRGBO(17, 16, 11, 1),
+                    buttonTextColor: const Color(0xFFEAE3D1),
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+            //Chat Textfield
+            Positioned(
               bottom: 0,
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 3,
-                          blurRadius: 30,
-                          offset: const Offset(0, 0.5),
-                        ),
-                      ],
-                      color: const Color(0xFFFFFFFF),
+              left: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 3,
+                      blurRadius: 30,
+                      offset: const Offset(0, 0.5),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: TextField(
-                        controller: messageController,
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: const Color(0xFFFFFFFF),
-                          enabledBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xFFFFFFFF)),
-                          ),
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Color(0xFFE6E6E6)),
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                          ),
-                          hintText: 'Ask anything...',
-                          hintStyle: const TextStyle(
-                            fontFamily: 'Montserrat',
-                            color: Color(0xAA000000),
-                            fontWeight: FontWeight.w600,
-                            fontSize: 12,
-                            letterSpacing: -0.41,
-                          ),
-                          suffixIcon: Transform.rotate(
-                            angle: -45 * math.pi / 180,
-                            child: const Icon(Icons.attachment_outlined),
-                          ),
-                          iconColor: const Color(0xFFCACACA),
-                          focusColor: const Color(0xFFCACACA),
-                        ),
+                  ],
+                  color: const Color(0xFFFFFFFF),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: TextField(
+                    controller: messageController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFFFFFFFF),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFFFFFFF)),
                       ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFE6E6E6)),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      hintText: 'Ask anything...',
+                      hintStyle: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Color(0xAA000000),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 12,
+                        letterSpacing: -0.41,
+                      ),
+                      suffixIcon: Transform.rotate(
+                        angle: -45 * math.pi / 180,
+                        child: const Icon(Icons.attachment_outlined),
+                      ),
+                      iconColor: const Color(0xFFCACACA),
+                      focusColor: const Color(0xFFCACACA),
                     ),
                   ),
                 ),
