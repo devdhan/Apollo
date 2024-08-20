0964bc0cb061fb401dd4faa0e5b142e552571719
Daniel <danielbolaji99@gmail.com>
2024-08-20 23:06:16 +0100

    okkk

--- tutorials/lib/pages/chat_two.dart
+++ tutorials/lib/pages/chat_two.dart
@@ -27,7 +27,6 @@ class ChatTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      primary: true,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(kToolbarHeight + 4),
         child: Container(
@@ -190,146 +189,158 @@ class ChatTwo extends StatelessWidget {
         ),
       ),
       backgroundColor: const Color.fromRGBO(234, 227, 209, 1),
-      body: SafeArea(
-        child: Stack(
-          children: <Widget>[
-            // Background image
-            Image.asset(
-              'assets/image.png',
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: double.infinity,
-            ),
-            // Text at the top
-            const Positioned(
-              top: 15.0,
-              left: 0,
-              right: 0,
-              child: Center(
-                child: Text(
-                  'Ask me anything',
-                  style: TextStyle(
-                      fontSize: 13,
-                      fontFamily: 'Montserrat',
-                      color: Color(0xA4000000)),
-                ),
-              ),
-            ),
-            // First container with text
-            Positioned(
-              top: 50.0,
-              left: 150,
-              right: 15,
-              child: Container(
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(20),
-                        bottomLeft: Radius.circular(30)),
-                    color: Color(0xFF11100B)),
-                child: const Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
-                  child: Text(
-                    'Explain the theory of evolution in details and give various examples',
-                    style: TextStyle(
-                        fontSize: 10,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFFFFFFFF)),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 130.0,
-              left: 15,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+      body: Stack(
+        children: [
+          // Background image
+          Image.asset(
+            'assets/image.png',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          // Scrollable content
+          Positioned.fill(
+            child: SingleChildScrollView(
+              child: Column(
                 children: [
-                  Image.asset(
-                    'assets/blankcircle.png',
+                  // Text at the top
+                  const Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Center(
+                      child: Text(
+                        'Ask me anything',
+                        style: TextStyle(
+                            fontSize: 13,
+                            fontFamily: 'Montserrat',
+                            color: Color(0xA4000000)),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(165, 20, 15, 0),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(20),
+                              bottomLeft: Radius.circular(30)),
+                          color: Color(0xFF11100B)),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 14.0, vertical: 10.0),
+                        child: Text(
+                          'Explain the theory of evolution in details and give various examples',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFFFFFFFF)),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Chat icon
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/blankcircle.png',
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 10, 165, 0),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(30),
+                              bottomRight: Radius.circular(30),
+                              bottomLeft: Radius.circular(20)),
+                          color: Color(0xFFFFFFFF)),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 14.0, vertical: 10.0),
+                        child: Text(
+                          'To write your exam effectively, start by preparing thoroughly with regular study and practice of past papers. Ensure you understand the exam format and gather all necessary materials. On exam day, manage your time by reading instructions carefully, starting with easier questions, and allocating time for each section. Use elimination for multiple choice questions, be concise in short answers, and outline essays before writing. Review your work for errors, completeness, and clarity. For objective exams, read all options; for subjective exams, support arguments with evidence; for problem-solving exams, show all work and double-check calculations. Post-exam, reflect on your performance to improve for next time.',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF000000)),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
-            Positioned(
-              top: 170.0,
-              left: 15,
-              right: 150,
-              child: Container(
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(30),
-                        bottomRight: Radius.circular(30),
-                        bottomLeft: Radius.circular(20)),
-                    color: Color(0xFFFFFFFF)),
-                child: const Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
-                  child: Text(
-                    'To write your exam effectively, start by preparing thoroughly with regular study and practice of past papers. Ensure you understand the exam format and gather all necessary materials. On exam day, manage your time by reading instructions carefully, starting with easier questions, and allocating time for each section. Use elimination for multiple choice questions, be concise in short answers, and outline essays before writing. Review your work for errors, completeness, and clarity. For objective exams, read all options; for subjective exams, support arguments with evidence; for problem-solving exams, show all work and double-check calculations. Post-exam, reflect on your performance to improve for next time.',
-                    style: TextStyle(
-                        fontSize: 10,
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF000000)),
+          ),
+          // Bottom textfield
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 3,
+                    blurRadius: 30,
+                    offset: const Offset(0, 0.5),
                   ),
-                ),
+                ],
+                color: const Color(0xFFFFFFFF),
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: 3,
-                      blurRadius: 30,
-                      offset: const Offset(0, 0.5),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: TextField(
+                  controller: messageController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFFFFFFF),
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFFFFFFF)),
                     ),
-                  ],
-                  color: const Color(0xFFFFFFFF),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                  child: TextField(
-                    controller: messageController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: const Color(0xFFFFFFFF),
-                      enabledBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFFFFFFF)),
-                      ),
-                      focusedBorder: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xFFE6E6E6)),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                      hintText: 'Ask anything...',
-                      hintStyle: const TextStyle(
-                        fontFamily: 'Montserrat',
-                        color: Color(0xAA000000),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 12,
-                        letterSpacing: -0.41,
-                      ),
-                      suffixIcon: Transform.rotate(
-                        angle: -45 * math.pi / 180,
-                        child: const Icon(Icons.attachment_outlined),
-                      ),
-                      iconColor: const Color(0xFFCACACA),
-                      focusColor: const Color(0xFFCACACA),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFFE6E6E6)),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    hintText: 'Ask anything...',
+                    hintStyle: const TextStyle(
+                      fontFamily: 'Montserrat',
+                      color: Color(0xAA000000),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                      letterSpacing: -0.41,
+                    ),
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Transform.rotate(
+                          angle: -45 * math.pi / 180,
+                          child: const Icon(Icons.attachment_outlined),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: const BoxDecoration(
+                            color: Color(0xFF11100B),
+                            shape: BoxShape.circle,
+                          ),
+                          child: const Icon(Icons.mic_outlined,
+                              color: Color(0xFFEAE3D1)),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
